0f3f50c38032e322a85e3919a558f98d2a9fc716
Yrekk <codeby.yrekk@gmail.com>
2026-09-16 13:35:11 +0200

    refactor: remove legacy guild admin model shim

--- src/claviger/models/guild_admin_configuration_model.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility import for the administrative model package."""
-
-from claviger.models.admin.guild_admin_configuration_model import (
-    GuildAdminConfiguration,
-)
-
-__all__ = ["GuildAdminConfiguration"]
