1eec7bc36b8491b1de40706ab2039c3de0425b4f
Yrekk <codeby.yrekk@gmail.com>
2026-09-16 15:36:28 +0200

    refactor: remove legacy context definition model shim

--- src/claviger/models/context_definition_model.py
+++
@@ -1,8 +0,0 @@
-"""Compatibility import for the context-domain definition model."""
-
-from claviger.models.context.context_definition_model import (
-    ContextDefinition,
-    ContextValueType,
-)
-
-__all__ = ["ContextDefinition", "ContextValueType"]
