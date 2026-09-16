60420ebc881157d5c4e1428f5fe7e3b503fb9652
Yrekk <codeby.yrekk@gmail.com>
2026-09-16 13:57:24 +0200

    refactor: remove legacy admin reconciliation service shim

--- src/claviger/services/admin_configuration_reconciliation_service.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility import for the relocated administrative reconciliation service."""
-
-from claviger.services.admin.admin_configuration_reconciliation_service import (
-    AdminConfigurationReconciliationService,
-)
-
-__all__ = ["AdminConfigurationReconciliationService"]
