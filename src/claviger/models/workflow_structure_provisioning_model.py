48e8b8bc0d59e1e0b176097fd2ca80bb0a42716e
Yrekk <codeby.yrekk@gmail.com>
2026-09-16 15:18:22 +0200

    refactor: remove legacy workflow provisioning model shim

--- src/claviger/models/workflow_structure_provisioning_model.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility import for the workflow structure provisioning model."""
-
-from claviger.models.workflows.workflow_structure_provisioning_model import (
-    WorkflowStructureProvisioningResult,
-)
-
-__all__ = ["WorkflowStructureProvisioningResult"]
