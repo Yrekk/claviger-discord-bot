f4e893cf112e8a07bc5af3c6df7ce529bdb1c0fb
Yrekk <codeby.yrekk@gmail.com>
2026-09-16 15:56:50 +0200

    refactor: remove legacy runtime metrics model shim

--- src/claviger/models/guild_configuration_metrics_model.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility import for the runtime-domain model."""
-
-from claviger.models.runtime.guild_configuration_metrics_model import (
-    GuildConfigurationMetrics,
-)
-
-__all__ = ["GuildConfigurationMetrics"]
