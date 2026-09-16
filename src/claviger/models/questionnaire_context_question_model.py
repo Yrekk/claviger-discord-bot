853480a648afa9359428c2e2b1f6f132406079ea
Yrekk <codeby.yrekk@gmail.com>
2026-09-16 15:36:34 +0200

    refactor: remove legacy questionnaire context model shim

--- src/claviger/models/questionnaire_context_question_model.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility import for the context-domain questionnaire model."""
-
-from claviger.models.context.questionnaire_context_question_model import (
-    QuestionnaireContextQuestion,
-)
-
-__all__ = ["QuestionnaireContextQuestion"]
