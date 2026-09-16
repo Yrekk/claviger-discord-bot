dff6a5170ab58627b874fc08048c978b917de0dc
Yrekk <codeby.yrekk@gmail.com>
2026-09-16 14:25:34 +0200

    refactor: remove legacy catalog repository shim

--- src/claviger/repositories/catalog_definition_repository.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility import for the catalog repository."""
-
-from claviger.repositories.catalogs.catalog_definition_repository import (
-    CatalogDefinitionRepository,
-)
-
-__all__ = ["CatalogDefinitionRepository"]
