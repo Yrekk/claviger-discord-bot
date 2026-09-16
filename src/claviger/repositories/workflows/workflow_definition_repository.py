4bb4d74ded07911909dffb84952a15a7486b5b83
Yrekk <codeby.yrekk@gmail.com>
2026-09-16 14:51:49 +0200

    refactor: use canonical workflow definition models

--- src/claviger/repositories/workflows/workflow_definition_repository.py
+++ src/claviger/repositories/workflows/workflow_definition_repository.py
@@ -8,12 +8,12 @@ from claviger.database.connection import (
     DatabaseMissingError,
     DatabaseUnavailableError,
 )
-from claviger.models.catalog_definition_model import CatalogDefinition
-from claviger.models.context_definition_model import (
+from claviger.models.catalogs.catalog_definition_model import CatalogDefinition
+from claviger.models.context.context_definition_model import (
     ContextDefinition,
     ContextValueType,
 )
-from claviger.models.workflow_definition_model import (
+from claviger.models.workflows.workflow_definition_model import (
     WorkflowCatalogBinding,
     WorkflowChannelMode,
     WorkflowContextBinding,
