5fc59a0de1dc194fe5920f34ededa0930fd9c641
Yrekk <codeby.yrekk@gmail.com>
2026-09-16 13:57:33 +0200

    refactor: remove legacy admin discovery service shim

--- src/claviger/services/admin_structure_discovery_service.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility import for the relocated administrative discovery service."""
-
-from claviger.services.admin.admin_structure_discovery_service import (
-    AdminStructureDiscoveryService,
-)
-
-__all__ = ["AdminStructureDiscoveryService"]
