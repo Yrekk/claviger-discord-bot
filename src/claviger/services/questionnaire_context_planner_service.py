b6c368ad8137d4f3ba9624f9d927121625fb137b
Yrekk <codeby.yrekk@gmail.com>
2026-09-16 15:37:14 +0200

    refactor: remove legacy questionnaire context planner shim

--- src/claviger/services/questionnaire_context_planner_service.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility import for the relocated questionnaire context planner."""
-
-from claviger.services.context.questionnaire_context_planner_service import (
-    QuestionnaireContextPlanner,
-)
-
-__all__ = ["QuestionnaireContextPlanner"]
